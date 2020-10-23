8223166342a1077fb840c95f60decb16c822ef9a
Adityan2003 <65527938+Adityan2003@users.noreply.github.com>
2020-10-23 14:16:19 +0530

    Add files via upload

---
+++ Data types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char name = 'x';
+    string phrase = "sequence of characters";
+    int number = -10;
+    float num = 2.7;
+    double numb = 3.34343;
+    bool isMale = true  // word for true and false
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string characterName="Andy";
+   int age;
+   age = 109;
+    cout << "There was once a man named "<< characterName << endl;
+    cout << "He was " <<age<< " years old"<< endl;
+    cout << "He liked the name "<< characterName << endl;
+    cout << "He did not like being " <<age<< endl;
+    return 0;
+}
